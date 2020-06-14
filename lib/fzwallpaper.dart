ce7d802370d011a7717080c628ba8c6100f168f9
Frazile <frazile.com@gmail.com>
2020-06-14 14:57:22 +0530

    Minor Bugs Fixed, Added Dartdoc

--- lib/fzwallpaper.dart
+++ lib/fzwallpaper.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
@@ -7,29 +6,34 @@ import 'package:path_provider/path_provider.dart';
 class Fzwallpaper {
   static const MethodChannel _channel = const MethodChannel('fzwallpaper');
 
+  /// This function is returning the platform version of the device.
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  /// This function is setting the image to the home screen as a wallpaper.
   static Future<String> homeScreen() async {
     final String resultvar =
         await _channel.invokeMethod('HomeScreen', 'myimage.jpeg');
     return resultvar;
   }
 
+  /// This function is setting the image to the lock screen as a wallpaper.
   static Future<String> lockScreen() async {
     final String resultvar =
         await _channel.invokeMethod('LockScreen', 'myimage.jpeg');
     return resultvar;
   }
 
+  /// This function is setting the image to the home & lock both screen as a wallpaper.
   static Future<String> bothScreen() async {
     final String resultvar =
         await _channel.invokeMethod('Both', 'myimage.jpeg');
     return resultvar;
   }
 
+  /// This function is returning the image downloading progress.
   static Stream<String> imageDownloadProgress(String url) async* {
     StreamController<String> streamController = new StreamController();
     try {
